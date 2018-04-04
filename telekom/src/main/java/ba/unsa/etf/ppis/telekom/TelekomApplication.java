56cb0dbb63166ac1a01e7c00d8a6bfb0336c7e8f
Lejla Zecevic <zeceviclejla1@gmail.com>
2018-04-04 20:56:37 +0200

    Initial commit

---
+++ telekom/src/main/java/ba/unsa/etf/ppis/telekom/TelekomApplication.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.ppis.telekom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelekomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelekomApplication.class, args);
+	}
+}
---
+++ telekom/src/test/java/ba/unsa/etf/ppis/telekom/TelekomApplicationTests.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.ppis.telekom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelekomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
