5d9186fdc7a2d441511bbd90bab8dfbe75a1331d
Lejla Zečević <lzecevic1@etf.unsa.ba>
2018-04-11 23:20:08 +0200

    Supplier crud (#2)
    * Delete and create supplier
    
    * Update and get supplier methods

---
+++ telekom/src/main/java/ba/unsa/etf/ppis/telekom/controllers/SupplierController.java
@@ -0,0 +1,48 @@
+package ba.unsa.etf.ppis.telekom.controllers;
+
+import ba.unsa.etf.ppis.telekom.models.Supplier;
+import ba.unsa.etf.ppis.telekom.services.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/supplier")
+public class SupplierController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @PostMapping("/new")
+    public void createSupplier(@RequestBody Supplier supplier) {
+        supplierService.saveSupplier(supplier);
+    }
+
+    @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
+    public void deleteSupplier(@PathVariable("id") Long id) {
+        Supplier supplier = supplierService.findById(id);
+        if(supplier != null) {
+            supplierService.delete(supplier);
+        }
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public void updateSupplier(@RequestBody Supplier supplier) {
+        Supplier supplierForUpdate = supplierService.findById(supplier.getId());
+        if(supplierForUpdate != null) {
+            updateSupplier(supplier, supplierForUpdate);
+            supplierService.saveSupplier(supplierForUpdate);
+        }
+    }
+
+    @GetMapping(value = "/find/{id}")
+    public Supplier getSupplierById(@PathVariable("id") Long id) {
+        return supplierService.findById(id);
+    }
+
+    private void updateSupplier(@RequestBody Supplier supplier, Supplier supplierForUpdate) {
+        supplierForUpdate.setAddress(supplier.getAddress());
+        supplierForUpdate.setCategory(supplier.getCategory());
+        supplierForUpdate.setContracts(supplier.getContracts());
+        supplierForUpdate.setName(supplier.getName());
+        supplierForUpdate.setRatings(supplier.getRatings());
+        supplierForUpdate.setStatus(supplier.getStatus());
+    }
+}
--- telekom/src/main/java/ba/unsa/etf/ppis/telekom/services/SupplierService.java
+++ telekom/src/main/java/ba/unsa/etf/ppis/telekom/services/SupplierService.java
@@ -3,5 +3,14 @@ package ba.unsa.etf.ppis.telekom.services;
 import ba.unsa.etf.ppis.telekom.models.Supplier;
 import ba.unsa.etf.ppis.telekom.repositories.SupplierRepository;
 
+import java.util.Optional;
+
 public class SupplierService extends BaseService<Supplier, SupplierRepository> {
+    public void saveSupplier(Supplier supplier) {
+        repository.save(supplier);
+    }
+
+    public Supplier findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
