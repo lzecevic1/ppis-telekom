4e73bd64d37be207656664723117582eeecaed58
mulicvedad <mulic.vedad@gmail.com>
2018-05-03 12:17:23 +0200

    Added suppliers and services (#7)

--- telekom/src/main/java/ba/unsa/etf/ppis/telekom/DbLoader.java
+++ telekom/src/main/java/ba/unsa/etf/ppis/telekom/DbLoader.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.ppis.telekom;
 
 import ba.unsa.etf.ppis.telekom.models.Role;
+import ba.unsa.etf.ppis.telekom.models.Supplier;
+import ba.unsa.etf.ppis.telekom.models.TelekomService;
 import ba.unsa.etf.ppis.telekom.models.User;
 import ba.unsa.etf.ppis.telekom.services.RoleService;
+import ba.unsa.etf.ppis.telekom.services.ServiceForTelekomService;
+import ba.unsa.etf.ppis.telekom.services.SupplierService;
 import ba.unsa.etf.ppis.telekom.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +15,7 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +25,10 @@ public class DbLoader implements CommandLineRunner {
     private UserService userService;
     @Autowired
     private RoleService roleService;
+    @Autowired
+    private ServiceForTelekomService serviceForTelekomService;
+    @Autowired
+    private SupplierService supplierService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -53,6 +62,41 @@ public class DbLoader implements CommandLineRunner {
             role = roleService.find("SUPPLIER");
             user = new User("supplier", password,hashedPassword, "supplier@telekom.ba", "1111-1111-1111", User.Gender.FEMALE, role);
             userService.save(user);
+        }
+        if (serviceForTelekomService.count() == 0) {
+            TelekomService telekomService = new TelekomService(TelekomService.ServiceType.CABLE_TV, "Usluge kablovske televizije",
+                    "Emir Dzamkic", new BigDecimal(59), TelekomService.ServicePriority.MISSION_CRITICAL);
+            serviceForTelekomService.save(telekomService);
+
+            telekomService = new TelekomService(TelekomService.ServiceType.iNTERNET, "Usluge pristupa internetu",
+                    "Bojana Maric", new BigDecimal(32), TelekomService.ServicePriority.MISSION_CRITICAL);
+            serviceForTelekomService.save(telekomService);
+
+            telekomService = new TelekomService(TelekomService.ServiceType.TELEPHONY, "Usluge fiksne telefonije",
+                    "Jozo Peric", new BigDecimal(26), TelekomService.ServicePriority.MISSION_CRITICAL);
+            serviceForTelekomService.save(telekomService);
+        }
+        if (supplierService.count() == 0) {
+            Supplier supplier = new Supplier();
+            supplier.setName("Imtec doo");
+            supplier.setAddress("Zmaja od bosne 43");
+            supplier.setStatus(Supplier.SupplierStatus.ACTIVE);
+            supplier.setCategory(Supplier.SupplierCategory.MOBILE_DEVICES);
+            supplierService.save(supplier);
+
+            supplier = new Supplier();
+            supplier.setName("Cisco");
+            supplier.setAddress("Mozart street, Vienna, AUS");
+            supplier.setStatus(Supplier.SupplierStatus.ACTIVE);
+            supplier.setCategory(Supplier.SupplierCategory.NETWORK_EQUIPMENT);
+            supplierService.save(supplier);
+
+            supplier = new Supplier();
+            supplier.setName("Feniks");
+            supplier.setAddress("Odobasina 77");
+            supplier.setStatus(Supplier.SupplierStatus.ACTIVE);
+            supplier.setCategory(Supplier.SupplierCategory.TELEPHONY);
+            supplierService.save(supplier);
         }
     }
 }
